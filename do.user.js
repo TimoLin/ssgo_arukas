305d648c99e667f9b84179559cf7e75ce1b86cf2
TimoZhang <ztnuaa0211@gmail.com>
2018-03-19 22:35:49 +0800

    fixed many bugs for digitalocean script

--- do.user.js
+++ do.user.js
@@ -1,11 +1,13 @@
+
 // ==UserScript==
 // @name         DO
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Gnerate shadowsocks QRcode in arukas docker
 // @author       ztnuaa
 // @match        https://cloud.digitalocean.com/droplets*
 // @grant        GM_xmlhttpRequest
+// @require http://code.jquery.com/jquery-1.8.2.js
 // @homepage     https://github.com/TimoLin/ssgo_arukas
 // ==/UserScript==
 
@@ -19,20 +21,19 @@
 
 function init(){
 
-    //以下三个参数请根据自己情况修改
+    //以下参数请根据自己情况修改
       //--------[1]加密方式-------
     //var method="aes-256-cfb";
-    var method=“rc4-md5”；
+    var method="rc4-md5";
     //var method="m2crypto"；
     //var method="salsa20";
     //var method="chacha20";
-      //---[2]shadowsocks密码----
-    var password="password";
-      //---[3]本地端口默认是1080---   
+    //---[2]shadowsocks密码----
+    var password="yourpassword";
+      //---[3]本地端口默认是1080---
     var local_port="1080";
-      //---[4]服务器端口---------
-    var server_port="8980";
-
+      //---[4]服务器端口(自行修改)---
+    var server_port="8989";
       //---[5]协议---
     var protocol="origin";
       //---[6]协议参数---
@@ -41,39 +42,45 @@ function init(){
     var obfs = "http_simple";
       //---[8]混淆参数---
     var obfs_param = "";
-    
-    
+
+
     var tab = document.getElementsByTagName("table")[0];
     var rowLen=tab.rows.length;
     var drop_num=tab.rows.length; //你创建的droplets的个数
-    
+
     for(var n=1;n<=drop_num-1;n++){
     //var n=1;
     var ip_column=tab.rows[n].cells[1]; //ip所在列的数据
     var ip=ip_column.getElementsByClassName('click-to-copy-text')[0].innerHTML; //获取ip地址
-    
+
     var ssh='ssh root@'+ip+' -p 22';
 
     //var app_name=document.getElementsByClassName("c-info__data")[1].innerHTML;
     var app=tab.rows[n].cells[0];
     var app_name=app.getElementsByClassName('Resource-title  ')[0].innerText;
         //shadowsocks为shadowsocks配置文件（json文件）
-    var ss_config="\"server\":\""+ip+"\","+"\"server_port\":"+server_port+ ",\"local port\":"+local_port+", \"password\":\""+password+"\", \"timeout\":600, \"method\":\""+method+"\"， \"remarks\" :\""+app_name+ "\"}";
+    //var ss_config="\"server\":\""+ip+"\","+"\"server_port\":"+server_port+ ",\"local port\":"+local_port+", \"password\":\""+password+"\", \"timeout\":600, \"method\":\""+method+"\"， \"remarks\" :\""+app_name+ "\"}";
 
-    var ssr_config="\"server\":\""+ip+"\","+"\"server_port\":"+server_port+"\"local_address\":\"127.0.0.1\""+ ",\"local port\":"+local_port+", \"password\":\""+password+"\", \"timeout\":300, \"method\":\""+method+"\",protocol\":\""+protocol+"\",\"protocol_param\":\""+protocol_param+"\",\"obfs\":\""+obfs+"\",\"obfs_param\":\""+obfs_param+",\"remarks\" :\""+app_name+ "\"}";
+    var ssr_config="{\"server\":\""+ip+"\","+"\"server_port\":"+server_port+";\"local_address\":\"127.0.0.1\""+ ",\"local port\":"+local_port+", \"password\":\""+password+"\", \"timeout\":300, \"method\":\""+method+"\",\"protocol\":\""+protocol+"\",\"protocol_param\":\""+protocol_param+"\",\"obfs\":\""+obfs+"\",\"obfs_param\":\""+obfs_param+"\",\"remarks\" :\""+app_name+ "\"}";
 
     //ss字符串格式为  加密方式:密码@ip地址:远程端口
-    var ss_str=method+":"+password+"@"+ip+":"+server_port; //+"#"+btoa(app_name);
-    
-    var ssr=ip+":"+port+":"+protol+":"+method+":"+":"+obfs+":"+btoa(password)+"/?obfsparam="+btoa(obfsparm);
-    var ssr=btoa(config_ssr)
-    var ssr=config_ssr.replace(/+/,/-/);
-    var ssr=config_ssr.replace(/\//,/_/);
-    var ssr_urlsafebase64="ss:\/\/"+config_ssr
+    var ss_str=method+":"+password+"@"+ip+":"+server_port;//+"#"+btoa(app_name);
+
+    var base64pass=btoa(password);
+        base64pass=base64pass.replace(/=+/,"");
+    var base64obfsparm=btoa(obfs_param);
+        base64obfsparm=base64obfsparm.replace(/=+/,"");
+
+    var config_ssr=ip+":"+server_port+":"+protocol+":"+method+":"+obfs+":"+base64pass;//+"/?obfsparam="+btoa(obfs_param);
+        config_ssr=btoa(config_ssr);
+        config_ssr=config_ssr.replace(/=+/,"");
+        config_ssr=config_ssr.replace(/\+/,"-");
+        config_ssr=config_ssr.replace(/\//,"_");
+    var ssr_urlsafebase64="ssr:\/\/"+config_ssr;
 
     //btoa()为base64加密函数，js内置.
     //加密后的ss字符串: ss://base64加密处理
-    var ss_base64="ss:\/\/"+btoa(config_str);
+    //var ss_base64="ss:\/\/"+btoa(config_str);
 
     //用linux的同学可以通过这个命令在终端生成二维码，详见Refrence【2】
     var command_line_config="echo -n \"ss:\/\/\"\`echo -n "+ss_str+" | base64` | qr";
@@ -84,13 +91,7 @@ function init(){
       //调用google chart api，生成二维码，详见Refrence【3】
     var QR_code="https://chart.googleapis.com/chart?cht=qr&chs="+imagesize+"x"+imagesize+"&choe=UTF-8&chld=M|1&chl="+ssr_urlsafebase64+"";
     var img_src="<img src = \""+QR_code+"\" />";
-   // var content_qr=document.createElement("div");
-    //var title2="二维码菌";
-    //content_qr.className="c-info__row";
-    //content_qr.innerHTML="<div class=\"c-info__title\">"+title2+"</div>";
-   // content_qr.appendChild(QR_code);
-   // document.getElementsByClassName("c-info u-mgt34 u-mgb34")[0].appendChild(content_qr);
-    
+
     //添加一行表格元素，标题为“命令”，内容有三行，第一行为ssh登录命令，第二行为通过Linux终端生成二维码的命令，第三行为shadowsocks配置文件的内容
     var newTabrow=tab.insertRow(rowLen+n-1);
         newTabrow.id=app_name;
@@ -99,11 +100,11 @@ function init(){
     //content.className="c-info__row";
     //content.innerHTML="<div class=\"c-info__title\">"+title1+"</div><div class=\"c-info__data\"><div id=\"ember723\" class=\"u-mgt13 u-mgb13 ember-view\"><ul class=\"list list-unstyled\"> <li>"+ssh+"</li> <li>"+command_line_config+ "</li> <li>"+shadowsocks+"<li></ul></div></div> </div>";
     var newCol_2=newTabrow.insertCell(0);//添加命令内容
-        newCol_2.innerHTML="</div><div class=\"c-info__data\"><div id=\"ember723\" class=\"u-mgt13 u-mgb13 ember-view\"><ul class=\"list list-unstyled\"> <li>"+ssh+"</li> <li>"+command_line_config+ "</li> <li>"+ss+"<li></ul></div>";
+        newCol_2.innerHTML="</div><div class=\"c-info__data\"><div id=\"ember723\" class=\"u-mgt13 u-mgb13 ember-view\"><ul class=\"list list-unstyled\"> <li>"+ssh+ "</li> <li>"+ssr_config+"<li></ul></div>";
     var newCol_1=newTabrow.insertCell(1);//添加列名称
-        newCol_1.innerHTML=app_name;
+        newCol_1.innerHTML="";
     var newCol_3=newTabrow.insertCell(2);
-        newCol_3.innerHTML="";
+        newCol_3.innerHTML=app_name;
     var newCol_4=newTabrow.insertCell(3);
         newCol_4.innerHTML=img_src;
 
@@ -115,9 +116,11 @@ window.onload=button();
 
 //添加按钮，用来触发调用函数
 function button(){
-   $("#container").append('<a id="I_am_a_button"> 点我生成二维码</a>');
-    $("#I_am_a_button").click(function(){
+   //jQuery("#Tabs u-mb-4 droplet-tabs").append('<li id="I_am_a_button" class="is-active ember-view"><a>点我</a></li>');
+   var iamabutton=document.createElement("li");
+    iamabutton.innerHTML='<li id="I_am_a_button"><a><font color="blue">点我</font></a></li>';
+   document.getElementsByClassName("Tabs u-mb-4 droplet-tabs")[0].appendChild(iamabutton);
+   jQuery("#I_am_a_button").click(function(){
         init();
     });
 }
-
